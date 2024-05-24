ad37c83fa2e95f85001809ec7772dca7cce2f0b4
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-24 21:45:21 +0700

    refactor: Add LoginResult type and update FetchUserLogin to handle login response

--- src/utils/fetchUserLogin.ts
+++ src/utils/fetchUserLogin.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { UserLogin } from "@/types";
+import { LoginResult } from "@/types/userLogin.type";
 
 export const FetchUserLogin = async (data: UserLogin): Promise<number> => {
   const BACKEND_URL = process.env.BACKEND_URL as string;
@@ -18,6 +19,14 @@ export const FetchUserLogin = async (data: UserLogin): Promise<number> => {
         password: data.password,
       }),
     });
+
+    const result: LoginResult = await res.json();
+    // cookies().set("userId", result.user.id, { secure: true });
+    // cookies().set("username", result.user.username, { secure: true });
+    // cookies().set("email", result.user.email, { secure: true });
+    // cookies().set("role", result.user.role, { secure: true });
+    // cookies().set("accessToken", result.tokens.accessToken, { secure: true });
+    // cookies().set("refreshToken", result.tokens.refreshToken, { secure: true });
 
     return res.status;
   } catch (error) {
