887c352b753e5d67cdda095bd22584e2937a36f9
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-25 22:21:00 +0700

    refactor: Update GetUserSession to simplify access token retrieval and verification

--- src/utils/GetUserSession.ts
+++ src/utils/GetUserSession.ts
@@ -9,27 +9,18 @@ export const GetUserSession = async (): Promise<UserSession | null> => {
   // Get the environment variables
   const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET as string;
 
-  // Initialize the access token
-  let accessToken: string = "";
-
   // Get the refresh token
   const rt = cookies().get("rt");
   if (!rt) return null;
 
-  // Check the access token
-  const at = cookies().get("at");
-  if (at) {
-    accessToken = at.value;
-  } else {
-    const newAt = await FetchRefreshToken();
-    if (!newAt) return null;
-    accessToken = newAt;
-  }
+  // Get the access token
+  const at = await FetchRefreshToken();
+  if (!at) return null;
 
   // Verify the access token
   const token = Buffer.from(JWT_ACCESS_SECRET, "base64").toString("utf-8");
   const secret = new TextEncoder().encode(token);
-  const verify = await jwtVerify(accessToken, secret);
+  const verify = await jwtVerify(at, secret);
 
   // Fill the session
   const session: UserSession = {
