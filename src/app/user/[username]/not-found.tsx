e02af449ea681f5910ed66d3356fd68064787289
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-31 08:05:00 +0700

    refactor: Update user not found page and profile page components

---
+++ src/app/user/[username]/not-found.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { FooterLayout, HeaderLayout } from "@/components";
+import { HomeOutlined, WarningOutlined } from "@ant-design/icons";
+import { Button, Layout, Typography } from "antd";
+
+const { Content } = Layout;
+const { Text } = Typography;
+
+function UserNotFound() {
+  return (
+    <Content
+      style={{
+        flex: 1,
+        backgroundColor: "#9E140F",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "1rem",
+        textAlign: "center",
+      }}
+    >
+      <WarningOutlined
+        style={{ fontSize: 75, color: "#fff", marginBottom: 15 }}
+      />
+      <Text style={{ fontSize: 30, color: "#fff" }}>
+        Maaf, halaman ini tidak tersedia.
+      </Text>
+      <Text style={{ fontSize: 15, color: "#fff" }}>
+        Tautan yang Anda ikuti mungkin rusak, atau halaman tersebut mungkin
+        telah dihapus.
+      </Text>
+      <Button
+        type="primary"
+        shape="round"
+        icon={<HomeOutlined />}
+        size={"large"}
+        style={{ marginTop: 25, backgroundColor: "#E2B808", color: "black" }}
+        href="/"
+      >
+        Kembali ke Beranda
+      </Button>
+    </Content>
+  );
+}
+
+export default function NotFound() {
+  return (
+    <Layout style={{ minHeight: "100dvh" }}>
+      <HeaderLayout />
+      <UserNotFound />
+      <FooterLayout />
+    </Layout>
+  );
+}
--- src/utils/GetUserProfile.ts
+++ src/utils/GetUserProfile.ts
@@ -4,7 +4,7 @@ import { UserProfile } from "@/types";
 
 export async function GetUserProfile(
   username: string
-): Promise<UserProfile | string> {
+): Promise<UserProfile | number> {
   // Get the environment variables
   const BACKEND_URL = process.env.BACKEND_URL as string;
   const HEADER_API_KEY = process.env.HEADER_API_KEY as string;
@@ -22,16 +22,16 @@ export async function GetUserProfile(
       }
     );
 
-    // If not 200, return false
-    if (resProfile.status !== 200) return "Akun yang dicari tidak ditemukan!";
+    // Response is 200
+    if (resProfile.status === 200) {
+      const profile: UserProfile = await resProfile.json();
+      return profile;
+    }
 
-    // Get the user profile
-    const profile: UserProfile = await resProfile.json();
-
-    // Return the user profile
-    return profile;
+    // Return the status
+    return resProfile.status;
   } catch (error) {
     // Return false
-    return "Server sedang mengalami gangguan! Coba lagi nanti!";
+    return 500;
   }
 }
