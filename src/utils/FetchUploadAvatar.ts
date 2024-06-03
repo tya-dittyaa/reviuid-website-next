bdaaf4dcc716f37f9ad37a920770ce3ab3ac3cce
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-06-03 08:14:25 +0700

    refactor: Add avatar upload functionality to user settings page

---
+++ src/utils/FetchUploadAvatar.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function FetchUploadAvatar(formData: FormData): Promise<boolean> {
+  // Get the environment variables
+  const BACKEND_URL = process.env.BACKEND_URL as string;
+  const HEADER_API_KEY = process.env.HEADER_API_KEY as string;
+
+  // Get the access token
+  const at = cookies().get("at");
+  if (!at) return false;
+
+  try {
+    // Fetch the user avatar
+    const res = await fetch(`${BACKEND_URL}/users/update/avatar`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${at.value}`,
+        "x-api-key": HEADER_API_KEY,
+      },
+      body: formData,
+    });
+
+    // If not 200, return false
+    if (res.status !== 200) return false;
+
+    // Return true
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
