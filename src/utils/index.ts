98f36a2663570cc949c880dc62e5ce09562be019
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-06-03 14:01:59 +0700

    refactor: Organize auth and user utils into separate directories

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,2 @@
-import { CheckAvailableEmail, FetchChangeEmail } from "./FetchChangeEmail";
-import {
-  CheckAvailableUsername,
-  FetchChangeUsername,
-} from "./FetchChangeUsername";
-import { FetchRefreshToken } from "./FetchRefreshToken";
-import { FetchUploadAvatar } from "./FetchUploadAvatar";
-import { FetchUserLogin } from "./FetchUserLogin";
-import { FetchUserLogout } from "./FetchUserLogout";
-import { FetchUserRegister } from "./FetchUserRegister";
-import { GetUserProfile } from "./GetUserProfile";
-import { GetUserSession } from "./GetUserSession";
-
-export {
-  CheckAvailableEmail,
-  CheckAvailableUsername,
-  FetchChangeEmail,
-  FetchChangeUsername,
-  FetchRefreshToken,
-  FetchUploadAvatar,
-  FetchUserLogin,
-  FetchUserLogout,
-  FetchUserRegister,
-  GetUserProfile,
-  GetUserSession,
-};
+export * from "./auth";
+export * from "./user";
---
+++ src/utils/user/index.ts
@@ -0,0 +1,5 @@
+export * from "./profile/GetUserProfile";
+export * from "./setting/FetchChangeBiography";
+export * from "./setting/FetchChangeEmail";
+export * from "./setting/FetchChangeUsername";
+export * from "./setting/FetchUploadAvatar";
