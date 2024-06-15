37d8987481aa0800aedb5c7d96d434acf724a051
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-06-15 17:09:57 +0700

    refactor: Update metadata for auth pages
    Update the metadata for the authentication pages (forgot-password, login, logout, register) to include open graph information. This includes setting the title, description, URL, site name, and image for each page. The metadata follows the id_ID locale and is of type "website". This change is necessary to enhance the visibility and branding of the Reviu.ID website.

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,9 +1,27 @@
 import "@fontsource/poppins";
 import type { Metadata } from "next";
 
+const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL!;
+
 export const metadata: Metadata = {
   title: "Masuk",
   description: "Masuk ke akun Reviu.ID untuk melanjutkan.",
+  openGraph: {
+    title: "Masuk » Reviu.ID",
+    description: "Masuk ke akun Reviu.ID untuk melanjutkan.",
+    url: `${FRONTEND_URL}/login`,
+    siteName: "Reviu.ID",
+    images: [
+      {
+        url: `${FRONTEND_URL}/logo.png`,
+        alt: "Reviu.ID",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
