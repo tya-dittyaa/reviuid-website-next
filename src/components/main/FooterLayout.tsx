a66b745fbafddb22cf5b4efe72ca9b51b42c5246
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-06-15 17:45:09 +0700

    refactor: Update footer link to use Indonesian translation

--- src/components/main/FooterLayout.tsx
+++ src/components/main/FooterLayout.tsx
@@ -65,8 +65,8 @@ function Horizontal() {
         <Link href="/about-us" style={{ color: "white", listStyle: "none" }}>
           Tentang Kami
         </Link>
-        <Link href="/developer" style={{ color: "white", listStyle: "none" }}>
-          Developer
+        <Link href="/developers" style={{ color: "white", listStyle: "none" }}>
+          Pengembang
         </Link>
       </Space>
     </Flex>
