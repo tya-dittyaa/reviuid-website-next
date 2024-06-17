ced6fde14472760654d39b38120530f9bb1de355
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-06-17 19:48:47 +0700

    refactor: Update UserFilmCommentContext to use UserFilmCommentContext instead of UserCommentContext

--- src/context/film/UserFilmCommentContext.tsx
+++ src/context/film/UserFilmCommentContext.tsx
@@ -1,21 +1,21 @@
 import { FilmReviewValue } from "@/types";
 import { createContext, useContext } from "react";
 
-const UserCommentContext = createContext<FilmReviewValue | null>(null);
+const UserFilmCommentContext = createContext<FilmReviewValue | null>(null);
 
 const UserFilmCommentProvider: React.FC<{
   value: FilmReviewValue | null;
   children: React.ReactNode;
 }> = ({ value, children }) => {
   return (
-    <UserCommentContext.Provider value={value}>
+    <UserFilmCommentContext.Provider value={value}>
       {children}
-    </UserCommentContext.Provider>
+    </UserFilmCommentContext.Provider>
   );
 };
 
 const useUserFilmComment = () => {
-  return useContext(UserCommentContext);
+  return useContext(UserFilmCommentContext);
 };
 
 export { UserFilmCommentProvider, useUserFilmComment };
