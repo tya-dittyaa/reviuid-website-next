3d39930078b93efc2f6509bbe746ef65217f6aca
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-06-17 22:19:26 +0700

    refactor: Update user home and setting components to use named exports
    This commit updates the user home and setting components to use named exports instead of individual exports for each component. This improves code organization and makes it easier to import and use these components in other parts of the application.

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,15 +1,2 @@
-import UserDeleteAccount from "./setting/UserDeleteAccount";
-import UserEditAvatar from "./setting/UserEditAvatar";
-import UserEditBiography from "./setting/UserEditBiography";
-import UserEditEmail from "./setting/UserEditEmail";
-import UserEditPassword from "./setting/UserEditPassword";
-import UserEditUsername from "./setting/UserEditUsername";
-
-export {
-  UserDeleteAccount,
-  UserEditAvatar,
-  UserEditBiography,
-  UserEditEmail,
-  UserEditPassword,
-  UserEditUsername,
-};
+export * from "./home";
+export * from "./setting";
